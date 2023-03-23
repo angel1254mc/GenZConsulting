363111ee84295bd48f9ebd8c2911f4cc87a30c36
Angel Lopez Pol <angellopezpol1254@gmail.com>
2023-03-23 15:28:36 -0400

    apostrophe issue

--- src/components/CapabilityHero.js
+++ src/components/CapabilityHero.js
@@ -23,7 +23,7 @@ const CapabilityHero = () => {
             <a.h1 style={heroTrail[0]} className="text-4xl md:text-6xl font-medium text-white mt-36 md:w-3/5 xl:text-7xl 2xl:text-8xl xl:w-2/3">
             Empowering Your Success
             </a.h1>
-            <a.p style={heroTrail[1]} className="hero-subtitle-text text-lg sm:text-2xl xl:text-3xl text-white">Unlock your business's full potential with our comprehensive and personalized consulting services - Gen Z Consulting is your partner in navigating the path to growth and achievement.</a.p>
+            <a.p style={heroTrail[1]} className="hero-subtitle-text text-lg sm:text-2xl xl:text-3xl text-white">Unlock your business&apos;s full potential with our comprehensive and personalized consulting services - Gen Z Consulting is your partner in navigating the path to growth and achievement.</a.p>
             <Spacer></Spacer>
           </section>
     
